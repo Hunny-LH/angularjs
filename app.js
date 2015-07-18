343b676238879b18fe7c40538dd4c2bf6bb98104
liuhan <393803588@qq.com>
2015-07-18 11:26:06 +0800

    project struct modify

--- app.js
+++ app.js
@@ -25,12 +25,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'app')));
 
 
 // angular启动页
 app.get('/', function (req, res) {
-    res.sendfile('dist/index.html');
+    res.sendfile('app/index.html');
 });
 app.listen(8003);
 console.log("server is started !")
